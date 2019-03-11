014d08b04be68f9ba8fb2b23fdef0017aabd2393
Priyanshu Garg <punit1503@gmail.com>
2019-03-11 16:24:04 +0530

    Added Time stamp and formatted it

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketio =require('socket.io');
 const http = require('http');
 const Filter = require('bad-words')
+const {generateMessage} = require('./utils/message')
  
 const app = express();
 const server = http.createServer(app);
@@ -15,15 +16,15 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket)=>{
     console.log('New webSocket Connection')
-    socket.emit('message','Welcome!')
+    socket.emit('message',generateMessage('Welcome!'))
 
-    socket.broadcast.emit('message', 'A new user has joined')
+    socket.broadcast.emit('message', generateMessage('A new user has joined'))
     socket.on('sendMessage', (message, callback)=>{
         const filter = new Filter()
         if(filter.isProfane(message)){
             return callback('Profanity not allowed')
         }
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback();
     })
     socket.on('send-location', (location, callback)=>{
@@ -35,7 +36,7 @@ io.on('connection',(socket)=>{
     })
 
     socket.on('disconnect', ()=>{
-        io.emit('message', 'A user has left')
+        io.emit('message', generateMessage('A user has left'))
     })
 })
 
